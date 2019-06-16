7920921841b7f90bb33592a978535a169b09d63d
Elena <elena.st463@gmail.com>
2019-06-16 14:04:39 +0200

    Section 2 - Lecture 10
    Respond to player input

--- Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -14,12 +14,29 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Pick a number.");
         Debug.Log("Highest number is: " + max);
         Debug.Log("Lowest number is: " + min);
+        Debug.Log("I think your number is 500. Tell me if your number is higher or lower.");
+        Debug.Log("Press Up Arrow = Higher, Down Arrow = Lower, Enter = Correct.");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed");
+        }
+
     }
 }
