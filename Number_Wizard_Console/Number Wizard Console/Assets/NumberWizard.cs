03f63463e332f03ad1dda0880146bb1a7eddfd68
Elena <elena.st463@gmail.com>
2019-06-18 23:31:04 +0200

    Section 2 - Lecture 12
    Scope and context of variables

--- Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
-
         Debug.Log("Welcome to Number Wizard! Yey!");
         Debug.Log("Pick a number.");
         Debug.Log("Highest number is: " + max);
@@ -25,10 +26,16 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log(guess);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log(guess);
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
